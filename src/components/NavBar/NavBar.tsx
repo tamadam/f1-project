8c703b72ec2cc7e3b485827979a9bd2c7b622bbe
tamadam <60942087+tamadam@users.noreply.github.com>
2023-04-05 17:59:44 +0200

    adding language selector component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,17 @@
 import "./NavBar.css";
 import mainLogo from "../../assets/F1.png";
+import { useRef } from "react";
+import LanguageSelector from "../LanguageSelector/LanguageSelector";
 
 const NavBar = () => {
   return (
     <div className="navigation-bar">
       <div className="nav-item item-container-1">
-        <img src={mainLogo}></img>
+        <img className="main-logo" src={mainLogo}></img>
       </div>
-      <div className="nav-item item-container-2">Item2</div>
+      <div className="nav-item item-container-2">
+        <LanguageSelector />
+      </div>
       <div className="nav-item item-container-3">
         <span className="explore-button">
           <a href="#">Bejelentkezés</a>
