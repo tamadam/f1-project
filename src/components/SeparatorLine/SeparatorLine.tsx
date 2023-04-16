6038182917e87ed8a83f29aa63b4650e3d9be2cb
tamadam <60942087+tamadam@users.noreply.github.com>
2023-04-16 19:15:59 +0200

    separatorline component class rename

--- src/components/SeparatorLine/SeparatorLine.tsx
+++ src/components/SeparatorLine/SeparatorLine.tsx
@@ -1,7 +1,7 @@
 import "./SeparatorLine.css";
 
 const SeparatorLine = () => {
-  return <div className="separator-line"></div>;
+  return <div className="main-page-separator-line"></div>;
 };
 
 export default SeparatorLine;
