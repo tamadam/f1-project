ae47d90c96af44d60e70c850ddd0e9bc6bf482c9
tamadam <60942087+tamadam@users.noreply.github.com>
2023-04-16 21:03:27 +0200

    main card class rename

--- src/components/MainCard/MainCard.tsx
+++ src/components/MainCard/MainCard.tsx
@@ -1,5 +1,5 @@
-import backgroundF1nosub from "../../assets/f1back2.jpg";
 import "./MainCard.css";
+import backgroundF1nosub from "../../assets/f1back2.jpg";
 import EmailForm from "../EmailForm/EmailForm";
 import { useContext } from "react";
 import { LanguageContext } from "../../App";
@@ -8,22 +8,23 @@ const MainCard = () => {
   const language = useContext(LanguageContext);
 
   return (
-    <div className="main-card-container">
-      <div className="story-card-bg">
-        <img src={backgroundF1nosub} alt="" />
-        <div className="bg-color"></div>
+    <div className="main-page-main-card-container">
+      <div className="main-page-main-card-background">
+        <img src={backgroundF1nosub} alt="f1 cars background" />
+        <div className="main-page-main-card-color"></div>
       </div>
-      <div className="story-card-text-container">
-        <div className="card-text-main-title-container">
-          <h1 className="card-text-main-title">
+      <div className="main-page-main-card-text-container">
+        <div className="main-page-main-card-title-container">
+          <h1 className="main-page-main-card-title">
             {language.mainCard.mainTitle}
           </h1>
-          <p className="card-text-subtitle">{language.mainCard.mainSubtitle}</p>
-          <div className="first-steps-container">
-            <div className="form-title">
+          <p className="main-page-main-card-subtitle">
+            {language.mainCard.mainSubtitle}
+          </p>
+          <div className="main-page-main-card-first-steps-container">
+            <div className="main-page-main-card-email-form-title">
               <h3>{language.mainCard.firstStepsTitle}</h3>
             </div>
-            {/* Form container*/}
             <EmailForm />
           </div>
         </div>
