091de543e3dc391b82a2e72af5a24ecb559af050
tamadam <60942087+tamadam@users.noreply.github.com>
2023-04-16 19:00:02 +0200

    renaming main page footer classes

--- src/components/MainFooter/MainFooter.tsx
+++ src/components/MainFooter/MainFooter.tsx
@@ -1,22 +1,21 @@
 import "./MainFooter.css";
 import mainLogo from "../../assets/f1logomain3.png";
-import phoneLogo from "../../assets/phone10.png";
-import notebookLogo from "../../assets/computer3.png";
-import tabletLogo from "../../assets/tablet.png";
-import tabletPhoneLogo from "../../assets/tabletphone.png";
-import LanguageSelector from "../LanguageSelector/LanguageSelector";
-import { ReactNode, useContext } from "react";
+import { useContext } from "react";
 import { LanguageContext } from "../../App";
 
 const MainFooter = () => {
   const language = useContext(LanguageContext);
 
   return (
-    <div className="main-footer-container">
-      <div className="footer-logo-container">
-        <img src={mainLogo} alt="f1-dataset-logo" />
+    <div className="main-page-footer-container">
+      <div className="main-page-footer-logo-container">
+        <img
+          src={mainLogo}
+          alt="f1 dataset logo"
+          className="main-page-footer-logo"
+        />
       </div>
-      <div className="footer-redirect-links-container">
+      <div className="main-page-footer-redirect-links-container">
         <a href="#">{language.mainFooter.linkContactUs}</a>
         <a href="#">{language.mainFooter.linkHelpCenter}</a>
         <a href="#">{language.mainFooter.linkAccount}</a>
@@ -24,15 +23,15 @@ const MainFooter = () => {
         <a href="#">{language.mainFooter.linkSpeedTest}</a>
         <a href="#">{language.mainFooter.linkPrivacy}</a>
       </div>
-      <div className="footer-warning-text-container">
-        <span className="footer-warning-text">
+      <div className="main-page-footer-warning-text-container">
+        <span className="main-page-footer-warning-text">
           {language.mainFooter.warningMessage}{" "}
-          <mark className="footer-warning-important">
+          <mark className="main-page-footer-warning-important">
             {language.mainFooter.warningMessageImportant}
           </mark>
         </span>
       </div>
-      <div className="footer-copyright-container">
+      <div className="main-page-footer-copyright-container">
         <span>©2023 F1 Dataset Hungary.</span>
       </div>
     </div>
