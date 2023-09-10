2413f533bee01637e01ef7d693725b8f3b7d2750
tamadam <60942087+tamadam@users.noreply.github.com>
2023-09-10 17:30:27 +0200

    Fix scroll up issue on route change

--- src/LayoutHomePage/LayoutHomePage.tsx
+++ src/LayoutHomePage/LayoutHomePage.tsx
@@ -3,9 +3,15 @@ import "./LayoutHomePage.css";
 import NavBar from "./components/NavBar/NavBar";
 import { PATH_HOME_PAGE } from "../constants";
 import Footer from "./components/Footer/Footer";
+import { useEffect } from "react";
 
 const LayoutHomePage = () => {
   const location = useLocation();
+
+  // Scroll to the top of the page when the location changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return (
     <div className="home-page-container">
